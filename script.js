25f626da1c3ecc4952da781f18bd8648b45e8319
Willy Brandt <46223150+willybrandtleao@users.noreply.github.com>
2022-12-19 15:35:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,6 @@
-# Avaliação Sprint 2 - Programa de Bolsas Compass.uol / AWS e IFCE
 
-Avaliação da segunda sprint do programa de bolsas Compass.uol para formação em machine learning para AWS.
-
-
-Este projeto trata-se da escolha de uma API pública para ser consumida com NODEJS, em uma página HTML simples.
-
-Sobre a API pública e a conclusão da avaliação
-A escolhida foi a  VIA CEP que após a inserção do CEP retorna informações tais como: BAIRRO, RUA, CIDADE, ESTADO E NÚMERO CADASTRADO NO IBJE. 
-
-Tecnologias e linguagens utilizadas:
-HTML - JAVASCRIPT – API VIA CEP 
-
-1.	CRIAÇÃO DE PÁGINA SIMPLES .HTML
-
-<html>
-  <head>
-  <title>Consulta Informações por CEP </title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-
- 
-  </head>
-
-  <body>
-
-    <script type="text/javascript" src="script.js"></script> // Optei por inseriria chamada do script dentro do body
-
-  <h1>Seja bem vindo ao Busca CEP</h1>
-  <p> Insira o CEP que deseja realizar a consulta sem pontos e traços da seguintes maneira: 63010245 </p>    
   
-  <form method="get" action="."> // formulário para busca das informações através do CEP
-      <label>CEP:
-      <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
-             onblur="pesquisacep(this.value);" /></label><br />
-      <label>RUA:
-      <input name="rua" type="text" id="rua" size="60" /></label><br />
-      <label>BAIRRO:
-      <input name="bairro" type="text" id="bairro" size="40" /></label><br />
-      <label>CIDADE:
-      <input name="cidade" type="text" id="cidade" size="40" /></label><br />
-      <label>ESTADO:
-      <input name="uf" type="text" id="uf" size="2" /></label><br />
-      <label>NÚMERO IBGE:
-      <input name="ibge" type="text" id="ibge" size="8" /></label><br />
-    </form>
-  </body>
-
-  </html>
-
-
-  2. Script para busca através do CPF no VIA CEP
-
-  function limpa_formulário_cep() {
+function limpa_formulário_cep() {
         //Limpa valores do formulário de cep.
         document.getElementById('rua').value=("");
         document.getElementById('bairro').value=("");
@@ -68,7 +17,7 @@ function meu_callback(conteudo) {
         document.getElementById('cidade').value=(conteudo.localidade);
         document.getElementById('uf').value=(conteudo.uf);
         document.getElementById('ibge').value=(conteudo.ibge);
-    } 
+    } //end if.
     else {
         //CEP não Encontrado.
         limpa_formulário_cep();
@@ -106,13 +55,13 @@ function pesquisacep(valor) {
             //Insere script no documento e carrega o conteúdo.
             document.body.appendChild(script);
 
-        } 
+        } //end if.
         else {
             //cep é inválido.
             limpa_formulário_cep();
             alert("Formato de CEP inválido.");
         }
-    } 
+    } //end if.
     else {
         //cep sem valor, limpa formulário.
         limpa_formulário_cep();
