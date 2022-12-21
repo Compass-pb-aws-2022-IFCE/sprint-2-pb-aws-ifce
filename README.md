17435b8b6ab2636e24c07fec96b44ff8c83606af
Humbert010 <humbertosampaio.dev@gmail.com>
2022-12-21 00:20:27 -0300

    aplicação funcional

---
+++ README.md
@@ -0,0 +1,19 @@
+# Avaliação da Sprint 2 - Consumindo API
+## O que é uma API?
+Uma _application programming interface_ (API) é um conjunto de definições e protocolos que permitem a integração de sistemas e possibilitando a reutilização de seus conteúdos e funcionalidades por outros softwares.
+## Aplicação Escolhida
+para esta tarefa, utilizei a api [api.nasa.gov](https://api.nasa.gov/), espeficamente a sua feature da imagem do dia, a qual retorna uma imagem temática com sua respectiva descrição, variando de acordo com a data de acesso.
+## Na Prática
+
+1 . Após abrir o terminal na pasta onde se econtram os arquivos, instale as dependências necessárias com o comando:
+```
+npm i express express-handlebars  request nodemon body-parser 
+```
+2 . Em seguia, utilize o comando abaixo para iniciar a aplicação, a qual poderá ser visualizada pelo navegador no endereço _localhost:3000_.
+```
+npm start
+```
+## Exemplo:
+![20/12/2002](exemplo.png)
+
+##
--- server.js
+++ server.js
@@ -1,34 +1,42 @@
-const https = require('https')
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const axios = require('axios')
-const request  = require('express')
-const path = require('path')
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const path = require("path");
+const https = require("https");
 
-app.use(cors())
+// configuração do handle bars
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "handlebars",
+  exphbs.engine({ extname: "handlebars", defaultLayout: "main" })
+);
+app.set("view engine", "handlebars");
 
-app.get('/', async (req, res) =>{
-
-    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
-      let data = '';
-    
-      // Um bloco de dados foi recebido.
-      resp.on('data', (chunk) => {
+//requisição:
+app.get("/", async (req, res) => {
+  https
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", (resp) => {
+      let data = "";
+      resp.on("data", (chunk) => {
         data += chunk;
       });
-    
-      // Toda a resposta foi recebida. Exibir o resultado.
-      resp.on('end', () => {
-        let r = JSON.parse(data)
-        res.json(r.hdurl);
-        res.sendFile(path.join(__dirname + '/simple.html'))
-    
+
+      //Exibir o resultado:
+      resp.on("end", () => {
+        let requisicao = JSON.parse(data);
+        let img_info = {
+          url: requisicao.url,
+          descricao: requisicao.explanation,
+          data: requisicao.date,
+        };
+
+        // renderiza a página:
+        res.render("simple", { img_info });
       });
-    
-    }).on("error", (err) => {
+    })
+    .on("error", (err) => {
       console.log("Error: " + err.message);
     });
-})
+});
 
-app.listen('3000')
+app.listen("3000");
